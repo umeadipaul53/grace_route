39c349555634a0cdea778f205fb78045c42bcad9
umeadipaul53 <umeadipaul53@gmail.com>
2025-09-19 16:21:41 +0100

    updated cors

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 2206;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 3300;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const frontendURL = isProduction
+  ? "https://grace-route-real-estate-company.onrender.com"
+  : "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: frontendURL,
+    credentials: true, // must match with Axios `withCredentials: true`
+  })
+);
 
 const userRoutes = require("./routes/user_routes");
 const adminRoutes = require("./routes/admin_routes");
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "server up and running" });
+  res
+    .status(200)
+    .json({ message: "server up and running", data: isProduction });
 });
 
 app.use("/user", userRoutes);
